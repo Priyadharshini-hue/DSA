39f9f065a1c22e06057e0e7560bf5c2c4a654be8
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-07-29 21:02:08 +0530

    MakeAnagramByDeleting

---
+++ String/MakeAnagramByDeleting.java
@@ -0,0 +1,65 @@
+/*
+Anagram of String
+
+Given two strings S1 and S2 in lowercase, the task is to make them anagram. 
+The only allowed operation is to remove a character from any string. 
+Find the minimum number of characters to be deleted to make both the strings anagram. 
+Two strings are called anagram of each other if one of them can be converted into another by rearranging its letters.
+
+Example 1:
+
+Input:
+S1 = bcadeh
+S2 = hea
+Output: 3
+Explanation: We need to remove b, c
+and d from S1.
+Example 2:
+
+Input:
+S1 = cddgk
+S2 = gcd
+Output: 2
+Explanation: We need to remove d and
+k from S1.
+Your Task:
+Complete the function remAnagram() which takes two strings S1, S2 as input parameter, and returns minimum characters needs to be deleted.
+*/
+import java.util.*;
+
+class MakeAnagramByDeleting{
+	static int remAnagram(String s1, String s2){
+		HashMap<Character, Integer> m = new HashMap<>();
+		
+		for(int i = 0; i < s1.length(); i++){
+			if(m.containsKey(s1.charAt(i))){
+				m.put(s1.charAt(i), m.get(s1.charAt(i))+1);
+			}else{
+				m.put(s1.charAt(i), 1);
+			}
+		}
+		for(int i = 0; i < s2.length(); i++){
+			if(m.containsKey(s2.charAt(i)) && s1.indexOf(s2.charAt(i)) != -1){
+				m.put(s2.charAt(i), m.get(s2.charAt(i))-1);
+			}
+			else{
+				if(m.containsKey(s2.charAt(i))){
+				m.put(s2.charAt(i), m.get(s2.charAt(i))+1);
+			}else{
+				m.put(s2.charAt(i), 1);
+			}
+			}
+		}
+		System.out.println(m);
+		int sum = 0;
+		for(int num : m.values()){
+			sum+=Math.abs(num);
+		}
+		return sum;
+	}
+	public static void main(String[] args){
+		String s1 = "basgadhbfgvhads";
+		String s2 = "sjdhgvbjdsbhvbvd";
+		System.out.println(remAnagram(s1,s2));
+	}
+}
