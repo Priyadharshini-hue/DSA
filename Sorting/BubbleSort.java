ae38cd1c449c34a8508ee5ebb4f48541e9c35d38
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-08-03 23:04:41 +0530

    Bubble Sorting

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+/* 
+Bubble Sorting / sinking sort
+O(n2) & O(1)
+In-place, stable, adaptive(deplends on initial input's sequential order) & 
+*/
+import java.util.*;
+
+class BubbleSort{
+	public static void main(String[] args){
+	
+		int[] arr = {1,2,3,4};
+		
+		for(int i = 0; i < arr.length - 1; i++){ // n-1 iterations
+			boolean swapped = false;
+			for(int j = 0; j < arr.length - i - 1; j++){
+				if( arr[j] > arr[j+1] ){
+					int t = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = t;
+					swapped = true;
+				}
+			}
+			// flag for optimized bubble sort
+			if(!swapped){
+				break;
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+}
