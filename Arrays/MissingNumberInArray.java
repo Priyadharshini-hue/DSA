f53f9ad35f77c2c57f57c545c1594b0f9da230e6
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-08-04 16:46:12 +0530

    Missing number in an array of 1 to n

--- Arrays/MissingNumberInArray.java
+++ Arrays/MissingNumberInArray.java
@@ -2,11 +2,14 @@
 Missing number in array
 Input: arr[] = {1, 2, 4, 6, 3, 7, 8}, N = 7
 Output: 5
+Input: nums = [0,1]
+Output: 2
 */
 
 class MissingNumberInArray{
 	
 	static int find(int[] arr, int n){
+		// the sum of integers from 1 to n is n(n+1)/2 and product is n!
 		int sum = ((n+1)*(n+2))/2;
 		
 		for(int i = 0; i < n; i++){
