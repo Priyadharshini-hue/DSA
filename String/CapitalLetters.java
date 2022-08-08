4b40aa1760024ccb2935c9a965a35afed2d6007f
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-08-09 00:09:49 +0530

    capital letters or not

---
+++ String/CapitalLetters.java
@@ -0,0 +1,20 @@
+class CapitalLetters{
+	
+	static boolean check(String s){
+		
+		int count = 0;
+		
+		for(int i = 0; i < s.length(); i++){
+			if(Character.toUpperCase(s.charAt(i)) == s.charAt(i)){
+				count++;
+			}
+		}
+		return count == s.length() ? true : false;
+	}
+
+	public static void main(String[] args){
+		String word = "PRkIYA";
+		
+		System.out.println(check(word));
+	}
+}
