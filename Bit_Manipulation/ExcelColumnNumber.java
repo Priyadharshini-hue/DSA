16bad332f264052bc456afb70106cd5fc08b1597
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-09-04 19:59:43 +0530

    excel column number from column name

--- Bit_Manipulation/ExcelColumnNumber.java
+++ Bit_Manipulation/ExcelColumnNumber.java
@@ -1,23 +1,14 @@
 /*
 In excel sheets there are multiple rows and columns. The rows are numbered like 1, 2, 3…
 The columns are numbered like A, B, C,…X, Y, Z, AA, AB,...AY, AZ, BA,...ZY, ZZ, AAA, AAB,...
-
 Here the column numbers denote the following:
-
 A => 1
-
 B => 2
-
 C => 3
-
-.
-
+....
 X => 24
-
 Y => 25
-
 Z => 26
-
 Given an excel column number, find the numerical column number.
  */
 class ExcelColumnNumber {
