1a5ca1d353d9e6775c1b7a81f54ea187b7ec25fb
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-09-06 23:57:43 +0530

    updated msb

--- Bit_Manipulation/MostSignificantBit.java
+++ Bit_Manipulation/MostSignificantBit.java
@@ -1,9 +1,10 @@
 // most significant bit
 class MostSignificantBit{
     static int msb(int n){
-        int index = (int)(Math.log(n) / Math.log(2));
-        //System.out.println(index);
-        return 1 << index;
+        // log2(n) = log(n) / log 2
+        int index = (int)(Math.log(n) / Math.log(2)); 
+        // Left shift by index
+        return 1 << index; 
     }
     public static void main(String[] args) {
         int number = 10;
