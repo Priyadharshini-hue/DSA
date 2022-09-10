4c7bd54095c24e2117f48bbbcc5963d9b6f4a751
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-09-10 23:09:28 +0530

    Sieve of Eratosthenes algorithm

--- Bit_Manipulation/PrimesUptoN.java
+++ Bit_Manipulation/PrimesUptoN.java
@@ -1,26 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// Sieve of Eratosthenes algorithm
+
 class PrimesUptoN {
     // O(n * log(log(n)))
     static List<Integer> findPrimes(int n) {
         ArrayList<Integer> list = new ArrayList<>();
 
+        // Initialize array of size n+1, by default initialized to false
         boolean bool[] = new boolean[n + 1];
 
+        // Initialize the array from index 2 to n+1 as true
         for (int i = 2; i < n; i++) {
             bool[i] = true;
         }
 
-        for (int i = 2; i*i < n; i++) {
-            for (int j = i * i; j <= n; j+=i) {
-                if(bool[i] == true) {
+        // Traverse till n if i^2 less than n and mark the multiples of i as non-prime
+        for (int i = 2; i * i < n; i++) {
+            if (bool[i] == true) {
+                for (int j = i * i; j <= n; j += i) {
                     bool[j] = false;
                 }
             }
         }
-        for(int i = 2; i <= n; i++) {
-            if(bool[i] == true) {
+        for (int i = 2; i <= n; i++) {
+            if (bool[i] == true) {
                 list.add(i);
             }
         }
