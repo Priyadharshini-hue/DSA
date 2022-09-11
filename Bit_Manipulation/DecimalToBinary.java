f330889258b43238e2ab9b45366a2100742eb6ce
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-09-11 22:57:29 +0530

    decimal to binary using bit

--- Bit_Manipulation/DecimalToBinary.java
+++ Bit_Manipulation/DecimalToBinary.java
@@ -1,18 +1,41 @@
 class DecimalToBinary {
-    static StringBuilder toBinary(int n) {
-        StringBuilder str = new StringBuilder();
-        int l = 0;
+    /*
+     * static StringBuilder toBinary(int n) {
+     * StringBuilder str = new StringBuilder();
+     * int l = 0;
+     * 
+     * while (n > 0) {
+     * l = n % 2;
+     * n = n / 2;
+     * str.append(l);
+     * }
+     * return str.reverse();
+     * }
+     */
+    // Integer.toString(num,2) --> to binary
+    static String str = "";
 
-        while (n > 0) {
-            l = n % 2;
-            n = n / 2;
-            str.append(l);
+    static String toBinary(int n) {
+
+        // for (int i = 1 << 31; i > 0; i = i /2) { // o/p for 30 digits - str = "0"
+        // 0000000000000000000000000000111
+        // if ((n & i) != 0) {
+        // str += 1;
+        // } else {
+        // str += 0;
+        // }
+        // }
+    // Recursive using bit
+        if (n > 1) {
+            toBinary(n >> 1); // n/2
         }
-        return str.reverse();
+        str += n & 1; // n % 2
+
+        return str;
     }
 
     public static void main(String[] args) {
-        int number = 7;
+        int number = 6;
 
         System.out.println(toBinary(number));
     }
