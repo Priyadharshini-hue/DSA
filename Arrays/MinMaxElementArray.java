df461bbf66c8302e53c4c79685170eee52611129
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-09-20 23:07:28 +0530

    To find the min and max value in array by comparing in pairs

--- Arrays/MinMaxElementArray.java
+++ Arrays/MinMaxElementArray.java
@@ -1,7 +1,7 @@
 /* 
 Find minimum and maximum element in an array
-Example 1:
 
+Example 1:
 Input:
 N = 6
 A[] = {3, 2, 1, 56, 10000, 167}
@@ -10,37 +10,76 @@ min = 1, max =  10000
  
 
 Example 2:
-
 Input:
 N = 5
 A[]  = {1, 345, 234, 21, 56789}
 Output:
 min = 1, max = 56789
- 
-
-Your Task:  
-You don't need to read input or print anything. 
-Your task is to complete the function getMinMax() which takes the array A[] and its size N as inputs 
-and returns the minimum and maximum element of the array.
 */
-class MinMaxElementArray{
-	//O(n)
-	static void find(long[] arr, long length){
-		long min = arr[0], max = arr[0];
-		
-		for(int i = 1; i < length; i++){
-			if(max < arr[i]){
-				max = arr[i];
-			}else if(min > arr[i]){
-				min = arr[i];
-			}
-		}
-		System.out.println("max : " + max);
-		System.out.println("min : " + min);
+class MinMaxElementArray {
+	static class Pair {
+		long min;
+		long max;
 	}
 
-	public static void main(String[] args){
-		long[] arr = {3, 2, 1, 56, 10000, 167};
-		find(arr, arr.length);
+	static Pair getMaxMin(long[] arr, int n) {
+
+		Pair pair = new Pair();
+
+		// Initialize currentIndex to track the index
+		int currentIndex = 0;
+
+		// If the length of the array is even
+		if (n % 2 == 0) {
+			// Compare first elements to find the min and max
+			if (arr[0] > arr[1]) {
+				pair.min = arr[1];
+				pair.max = arr[0];
+			} else {
+				pair.min = arr[0];
+				pair.max = arr[1];
+			}
+			currentIndex = 2;
+		}
+		// if the length is odd
+		else {
+			// Assign the first element as min and max
+			pair.min = arr[0];
+			pair.max = arr[0];
+			currentIndex = 1;
+		}
+
+		// Loop till n by incrementing 2
+		while (currentIndex < n) {
+			// if the currentIndex element is less than the next element
+			if (arr[currentIndex] < arr[currentIndex + 1]) {
+				// if the currentIndex element is less than the min value
+				if (pair.min > arr[currentIndex]) {
+					pair.min = arr[currentIndex];
+				}
+				// if the currentIndex's next element is greater than max value
+				if (arr[currentIndex + 1] > pair.max) {
+					pair.max = arr[currentIndex + 1];
+				}
+			} 
+			// else 
+			else {
+				if (pair.min > arr[currentIndex + 1]) {
+					pair.min = arr[currentIndex + 1];
+				}
+				if (arr[currentIndex] > pair.max) {
+					pair.max = arr[currentIndex];
+				}
+			}
+			currentIndex += 2;
+		}
+		return pair;
+	}
+
+	public static void main(String[] args) {
+		long[] arr = { 3, 2, 56111166, 1, 56, 10000, 167 };
+		Pair value = getMaxMin(arr, arr.length);
+		System.out.println(value.max);
+		System.out.println(value.min);
 	}
 }
