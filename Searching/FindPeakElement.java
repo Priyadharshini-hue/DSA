63a5c76844110a38c37be2ccd7440485ed057633
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-09-28 15:49:06 +0530

    peak element in unsorted array

---
+++ Searching/FindPeakElement.java
@@ -0,0 +1,31 @@
+/*
+A peak element is an element that is strictly greater than its neighbors.
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Can return either index number 1 where the peak element is 2, 
+or index number 5 where the peak element is 6.
+ */
+class FindPeakElement {
+
+    static int findElement(int[] arr, int n) {
+        int start = 0, end = n;
+
+        while (start<end) {
+            int mid = start + (end -start)/2;
+
+            if (arr[mid] > arr[mid+1]) {
+                end = mid;
+            } else {
+                start = mid+1;
+            }
+        }
+        return end;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1,2,1,3,5,6,4};
+
+
+        System.out.println(findElement(arr, arr.length - 1));
+    }
+}
