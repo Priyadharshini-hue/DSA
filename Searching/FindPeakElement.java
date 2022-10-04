47c9ccb500fc39121b2e3ad61eb2be06f72b5257
priyadharshini-hue <priyadharshini.ra98@gmail.com>
2022-10-04 19:39:22 +0530

    Find element greater than adjacent elements

--- Searching/FindPeakElement.java
+++ Searching/FindPeakElement.java
@@ -7,25 +7,28 @@ or index number 5 where the peak element is 6.
  */
 class FindPeakElement {
 
-    static int findElement(int[] arr, int n) {
-        int start = 0, end = n;
+    static int findElement(int[] arr) {
+        
+        int start = 0, end = arr.length - 1;
 
-        while (start<end) {
-            int mid = start + (end -start)/2;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
 
-            if (arr[mid] > arr[mid+1]) {
+            // Check if the mid is greater than next element, then ignore right half
+            if (arr[mid] > arr[mid + 1]) {
                 end = mid;
-            } else {
-                start = mid+1;
+            }
+            // If the mid is less than next element, then ignore left half
+            else {
+                start = mid + 1;
             }
         }
         return end;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 1,2,1,3,5,6,4};
+        int arr[] = { 1, 2, 1, 3, 5, 6, 4 };
 
-
-        System.out.println(findElement(arr, arr.length - 1));
+        System.out.println(findElement(arr));
     }
 }
