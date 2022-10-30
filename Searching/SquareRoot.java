634592c678ba83625874ed275c1bb8278941bf5b
Priyadharshini-hue <priyadharshini.tnj98@gmail.com>
2022-10-30 22:48:43 +0530

    square root

--- Searching/SquareRoot.java
+++ Searching/SquareRoot.java
@@ -1,38 +1,29 @@
-class SquareRoot {
-    public static int root(int number) {
-        // Base case
-        // If the number is 0 or 1 then the sqaure root is same as number
-        if (number == 0 || number == 1) {
-            return number;
+// is perfect square-- limit = 1to10000.
+public class SquareRoot {
+    static int findSquareRoot(int x) {
+        if (x == 0) {
+            return 0;
         }
-
-        // Initialize start, end and res
-        long start = 1, end = number / 2, res = 1;
+        int start = 1, end = x / 2, mid, res = 1;
 
         while (start <= end) {
-            // Initialize mid
-            long mid = start + (end - start) / 2;
+            mid = start + (end - start) / 2;
 
-            // If the mid* mid equals number, return mid
-            if (mid * mid == number) {
-                return (int) mid;
-            }
-            // If the mid*mid less than number, then res is mid & start is mid's next
-            // element
-            else if (mid * mid < number) {
+            if (x == mid * mid) {
+                return mid;
+            } else if (x < mid * mid) {
+                end = mid - 1;
+            } else {
                 start = mid + 1;
                 res = mid;
             }
-            // If greater then end is mid's previous element
-            else {
-                end = mid - 1;
-            }
         }
-        return (int) res;
+        return res;
     }
 
     public static void main(String[] args) {
-        int number = 180;
-        System.out.println(root(number));
+        int number = 15; // 3
+
+        System.out.println(findSquareRoot(number));
     }
 }
