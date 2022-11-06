6c84af0fd1276416be15243a0c2e77929742f09d
Priyadharshini <priyadharshini.tnj98@gmail.com>
2022-11-06 23:26:18 +0530

    segregate odd and even indices

---
+++ Linked List/SegregateOddEvenIndices.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+class LinkedList {
+    Node head;
+
+    // Insert at end
+    void insertAtEnd(int data) {
+        Node node = new Node(data);
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+        }
+    }
+
+    void segregateOddEvenIndices() {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        // first node is odd,second node is even
+        Node odd = head, even = head.next,
+                evenStart = even;
+        while (odd.next != null && even.next != null) {
+            odd.next = even.next;
+            odd = even.next;
+            even.next = odd.next;
+            even = odd.next;
+        }
+        odd.next=evenStart;
+    }
+
+    // Print the list
+    void print() {
+        Node currentNode = head;
+
+        if (head == null) {
+            return;
+        } else {
+            while (currentNode != null) {
+                System.out.print(currentNode.data + " ");
+                currentNode = currentNode.next;
+            }
+        }
+        System.out.println(" ");
+    }
+}
+
+public class SegregateOddEvenIndices {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        try (Scanner s = new Scanner(System.in)) {
+            int limit = s.nextInt();
+
+            for (int i = 0; i < limit; i++) {
+                int data = s.nextInt();
+                list.insertAtEnd(data);
+            }
+        }
+        list.segregateOddEvenIndices();
+        list.print();
+    }
+}
