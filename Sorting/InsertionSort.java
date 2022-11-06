f319820e426ae1281c623835a18236e09e9fd118
Priyadharshini <priyadharshini.tnj98@gmail.com>
2022-11-06 23:22:55 +0530

    insertion sort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,24 +1,34 @@
-/*
-Insertion sort
-O(n2) & O(1)
-in-place, stable adaptive
-*/
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-class InsertionSort{
-	public static void main(String[] args){
-		int[] arr = {4, 2, 5, 3, 7, 1};
-		
-		for(int i = 1; i < arr.length; i++){
-			int key = arr[i];
-			int j = i -1;
-			
-			while(j>=0 && key < arr[j]){
-				arr[j+1] = arr[j];
-				j--;
-			}
-			arr[j+1] = key;
-		}
-		System.out.println(Arrays.toString(arr));
-	}
+public class InsertionSort {
+
+    static void sort(int[] arr) {
+        int key;
+        for (int i = 1; i < arr.length; i++) {
+            key = arr[i];
+            int j = i - 1;
+
+            while (0 <= j && key < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner s = new Scanner(System.in)) {
+            int length = s.nextInt();
+            int[] arr = new int[length];
+
+            for (int i = 0; i < length; i++) {
+                arr[i] = s.nextInt();
+            }
+
+            sort(arr);
+
+            System.out.println(Arrays.toString(arr));
+        }
+    }
 }
