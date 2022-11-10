1e17bec61df46171bdec217c973a5e54f682ec56
Priyadharshini <priyadharshini.tnj98@gmail.com>
2022-11-10 23:51:56 +0530

    insertion in DLL

---
+++ Linked List/InsertionDLL.java
@@ -0,0 +1,155 @@
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next, prev;
+
+    Node(int d) {
+        data = d;
+        next = null;
+        prev = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    // Insert at end
+    void insertAtEnd(int data) {
+        Node node = new Node(data);
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+            node.prev = currentNode;
+        }
+    }
+
+    // Insert at start
+    void insertAtStart(int data) {
+        Node node = new Node(data);
+
+        if (head == null) {
+            head = node;
+        } else {
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+    }
+
+    void insertAtMiddle(int data) {
+        Node node = new Node(data);
+
+        if (head == null) {
+            head = node;
+        }
+        /*
+         * 1 2 3 _ 4 5 -odd
+         * 1 2 _ 3 4 -even (first of the two middle elements.)
+         */
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        node.next = slow.next;
+        node.prev = slow;
+        slow.next = node;
+        node.next.prev = node;
+    }
+
+    void insertAtKthPosition(int data, int k) {
+        Node node = new Node(data);
+
+        if (k == 0) {
+            return;
+        }
+
+        // head insertion if k is 1
+        if (k == 1) {
+            node.next = head;
+            head.prev = node;
+            head = node;
+            return;
+        }
+
+        Node currentNode = head, prevNode = null;
+        while (1 < k && currentNode != null) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+            k--;
+        }
+
+        if (currentNode == null & k > 1) {
+            return;
+        }
+
+        // After k
+        // Node currentNode = head, prevNode = null;
+        // while (1 <= k && currentNode != null) {
+        // prevNode = currentNode;
+        // currentNode = currentNode.next;
+        // k--;
+        // }
+
+        // if (currentNode == null & k >1) {
+        // return;
+        // }
+
+        node.next = prevNode.next;
+        node.prev = prevNode;
+        prevNode.next = node;
+
+        if (node.next != null) {
+            node.next.prev = node;
+        }
+    }
+
+    // Print the list
+    void print() {
+        Node currentNode = head;
+
+        if (head == null) {
+            return;
+        } else {
+            while (currentNode != null) {
+                System.out.print(currentNode.data + " ");
+                currentNode = currentNode.next;
+            }
+        }
+        System.out.println(" ");
+    }
+}
+
+public class InsertionDLL {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        try (Scanner s = new Scanner(System.in)) {
+            int limit = s.nextInt();
+
+            for (int i = 0; i < limit; i++) {
+                int data = s.nextInt();
+                list.insertAtEnd(data);
+            }
+        }
+        System.out.println("----------end-------------");
+        list.print();
+        System.out.println("-----------start------------");
+        list.insertAtStart(0);
+        list.print();
+        System.out.println("-----------middle------------");
+        list.insertAtMiddle(0);
+        list.print();
+        System.out.println("-----------kth------------");
+        list.insertAtKthPosition(100, 8);
+        list.print();
+        System.out.println("-----------------------");
+    }
+}
