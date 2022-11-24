aaac612c93ef5658311cf2a671ee33179fa88cd3
Priyadharshini <priyadharshini.tnj98@gmail.com>
2022-11-24 19:58:54 +0530

    largest number in array

--- Arrays/LargestNumber.java
+++ Arrays/LargestNumber.java
@@ -1,54 +1,29 @@
 /*
 Largest Element in Array
-
-Given an array A[] of size n. The task is to find the largest element in it.
-
-Example 1:
-
 Input:
 n = 5
 A[] = {1, 8, 7, 56, 90}
 Output:
 90
-Explanation:
-The largest element of given array is 90.
- 
-
-Example 2:
-
-Input:
-n = 7
-A[] = {1, 2, 0, 3, 2, 4, 5}
-Output:
-5
-Explanation:
-The largest element of given array is 5.
- 
-
-Your Task:  
-You don't need to read input or print anything. Your task is to complete the function largest() which takes the array A[] and its size n as inputs and returns the maximum element in the array.
-
- 
-
-Expected Time Complexity: O(N)
-Expected Auxiliary Space: O(1)
 */
 
-class LargestNumber{
-	//O(n)
-	static int maxNum(int[] arr, int length){
-		int max = arr[0];
-		for(int i = 0; i < length; i++){
-			if( arr[i] > max){
-				max = arr[i];
+class LargestNumber {
+	// O(n)
+	static int maxNum(int[] arr, int length) {
+		int left = 0, right = length - 1, max = -1;
+		while (left < right) {
+			if (arr[left] < arr[right]) {
+				left++;
+			} else {
+				right--;
 			}
 		}
-		return (max);
+		return (arr[left]);
 	}
-	
-	public static void main(String[] args){
-	int[] arr = {1, 2, 0, 3, 2, 4, 5};
-	
-	System.out.println(maxNum(arr, arr.length));
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 0, 3, 2, 4, 5 };
+
+		System.out.println(maxNum(arr, arr.length));
 	}
 }
