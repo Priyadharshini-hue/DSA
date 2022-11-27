91e61ced419e3d5c1ef91cdaf0be03db0609c943
Priyadharshini <priyadharshini.tnj98@gmail.com>
2022-11-27 20:38:53 +0530

    rotate left

--- Arrays/RotateLeft.java
+++ Arrays/RotateLeft.java
@@ -1,11 +1,18 @@
-//Rotate array to left by n
 
-import java.util.*;
+/*
+AntiClock-wise direction(left side).
+Input:
+N = 7, k=2
+A[] = {1, 2, 3, 4, 5,6,7}
+Output:
+3 4 5 6 7 1 2
+*/
+import java.util.Scanner;
 
-class RotateLeft{
-	
-	static void reverse(int[] arr, int left, int right){
-		while(left <= right){
+class RotateLeft {
+
+	static void reverse(int[] arr, int left, int right) {
+		while (left < right) {
 			int t = arr[left];
 			arr[left] = arr[right];
 			arr[right] = t;
@@ -13,19 +20,26 @@ class RotateLeft{
 			right--;
 		}
 	}
-	
-	static void rotate(int[] arr, int l, int shift){
-		reverse(arr, 0, l-shift-1);
-		reverse(arr, l-shift, l-1);
-		reverse(arr, 0, l-1);
+
+	static void rotate(int[] arr, int l, int k) {
+		k = k % l;
+		reverse(arr, 0, l - 1);
+		reverse(arr, 0, l - k - 1);
+		reverse(arr, l - k, l - 1);
 	}
 
-	public static void main(String[] args){
-		int[] arr = {1, 2, 3, 4, 5, 6}; // 0 1 2 3 4 5 ,6
-		int rotate = 3;
-		
-		rotate(arr, arr.length, rotate);
-		
-		System.out.println(Arrays.toString(arr));
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int k = s.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.nextInt();
+		}
+
+		rotate(arr, n, k);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 }
